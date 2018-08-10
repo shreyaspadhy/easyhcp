0b23aed7e4c5b379a9c800f09976ca153e6f3c01
Patrick Beukema <beuk.pat@gmail.com>
2018-08-10 13:49:44 -0700

    Update hcpscraper.py
    removed unnecessary class

--- easyhcp/hcpscraper.py
+++ easyhcp/hcpscraper.py
@@ -1,14 +1,11 @@
-import boto
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import boto3
 import os
 import json
 import subprocess
 import sklearn as sk
-
-class User():
-    def __init__(self, access_key, secret_key):
-        self.access_key = access_key
-        self.secret_key = secret_key
-
 
 def setup_credentials():
     """
